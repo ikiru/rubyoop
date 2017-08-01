b06736eca7754b188f26d1d2b95efb8b9be157bb
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-01 15:00:33 -0500

    math

--- project.rb
+++ project.rb
@@ -11,7 +11,3 @@ class Project
     puts 'description'
   end
 end
-
-project1 = Project.new('Project 1', 'Description 1')
-puts project1.name # => "Project 1"
-project1.elevator_pitch # => "Project 1, Description 1"
