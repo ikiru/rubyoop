6f521b041826290666ba7dfe51da2b3080da4f19
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-02 08:04:57 -0500

    oop

---
+++ dog.rb
@@ -0,0 +1,23 @@
+require_relative 'mammal'
+
+class dog < Mammal
+
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+
+  def pet
+
+  end
+
+  def walk
+
+  end
+
+  def run
+
+  end
+
+end
---
+++ mammal.rb
@@ -0,0 +1,16 @@
+class Dog < Mammal
+  def pet
+    @health += 5
+    self
+  end
+
+  def walk
+    @health -= 1
+    self
+  end
+
+  def run
+    @health -= 10
+    self
+  end
+end
